7167fc9fa9bf19f5e6270a4c814d74111a753a00
alireza vafadar <alirezavafadar.ca@gmail.com>
2022-07-16 00:26:49 -0400

    after styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 import Coin from './coin';
-import { Container, Row, Col, Form} from 'react-bootstrap';
+import { Container, Row, Col, Form, Nav, Navbar} from 'react-bootstrap';
 
 function App() {
 
@@ -30,36 +30,48 @@ function App() {
   )
 
   return(
-    <Container>
-      <Row>
-        <Col  xs={10} md={6}>
-          <h1 className='acronym'>Crypto Price</h1>
-          <Form>
-          <Form.Group className="mb-3">
-            <Form.Control type="search" placeholder="search a currency" onChange={handleChange}/>
-          </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-      <Row>
-        
-          {filteredCoins.map(coin => {
-          return <Col xs={12} md={4}>
-            <Coin key={coin.id}
-            name={coin.name} 
-            image={coin.image} 
-            symbol={coin.symbol}
-            marketcap={coin.market_cap}
-            price={coin.current_price}
-            priceChange={coin.price_change_percentage_24h}
-            volume={coin.total_volume}
-
-          />
+    <React.Fragment>
+      <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      <Container>
+        <Row className='coin-search'>
+          <Col  xs={10} md={6}>
+            <h1 className='head'>Crypto Price</h1>
+            <Form className='coin-input' >
+            <Form.Group className="mb-3">
+              <Form.Control  className='coin-input' type="text" placeholder="search a currency" onChange={handleChange}/>
+            </Form.Group>
+            </Form>
           </Col>
-          })}
-        
-      </Row>
-    </Container>
+        </Row>
+        <Row>
+          
+            {filteredCoins.map(coin => {
+            return <Col xs={12} md={6} lg={4}>
+              <Coin key={coin.id}
+              name={coin.name}
+              image={coin.image} 
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              price={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              volume={coin.total_volume}
+
+            />
+            </Col>
+            })}
+          
+        </Row>
+      </Container>
+    </React.Fragment>
   )
 }
 
--- src/coin.js
+++ src/coin.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import './Coin.css'
-import {Card,Button} from 'react-bootstrap'
+import {Card,Button, Row, Col} from 'react-bootstrap'
 import Image from 'react-bootstrap/Image'
 
 function Coin({image, name, symbol, price, volume, priceChange, marketcap}) {
     return ( 
         
-                <Card bg="primary">
-                    <Image shape="thumbnail" height="100px" width="100px" src={image} alt='crypto-logo'/>
-                    <Card.Body>
-                        <Card.Title>{name}</Card.Title>
-                        <Card.Text>{symbol}</Card.Text>
-                        <Card.Text>${price}</Card.Text>
-                        <Card.Text>${volume.toLocaleString()}</Card.Text>
-                        {priceChange < 0 ? (   
+                <Card bg="dark" className='card'>
+                    <Row>
+                        <Col  xs={5} md={4}>
+                            <Image  className="coin-image" shape="thumbnail" height="80px" width="80px" src={image} alt='crypto-logo'/>
+                        </Col>
+                        <Col className='coin-col' xs={7} md={8}>
+                            <Card.Title className='coin-title'>{name}</Card.Title>
+                            <Card.Text className='coin-symbol'>{symbol.toUpperCase()}</Card.Text>
+                        </Col>
+                    </Row>
+                    <Card.Text className='coin-price white'>USD {price}</Card.Text>
+                    {priceChange < 0 ? (   
                             <Card.Text className='coin-percent red'>{priceChange.toFixed(2)}%</Card.Text>
                         ) : (<Card.Text className='coin-percent green'>{priceChange.toFixed(2)}%</Card.Text>
                         )}
-                        <Card.Text>Market Cap: ${marketcap.toLocaleString()}</Card.Text>
+                    <Card.Body>
+                        <Card.Text className='white'>Volume: ${volume.toLocaleString()}</Card.Text>
+                        <Card.Text className='white'>Market Cap: ${marketcap.toLocaleString()}</Card.Text>
                         <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
                 </Card>
